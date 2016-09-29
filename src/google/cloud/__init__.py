ccdd5f2a12851bd801d89c7fac9e61a7edeab71c
Danny Hermes <daniel.j.hermes@gmail.com>
2016-09-29 10:13:23 -0700

    Bringing scripts/ up to date with upstream.

--- src/google/cloud/__init__.py
+++ src/google/cloud/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc.
+# Copyright 2016 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Google Cloud API access in idiomatic Python."""
 
 try:
     __import__('pkg_resources').declare_namespace(__name__)

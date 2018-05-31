193e3e97983bae0377c11f6e9a18423c4695d682
vikas-jamdar <vikas@qlogic.io>
2018-06-01 01:25:54 +0530

    Updated Happy Base framework to the latest version of Bigtable 0.29.0

--- setup.py
+++ setup.py
@@ -51,14 +51,14 @@ SETUP_BASE = {
 
 
 REQUIREMENTS = [
-    'google-cloud-bigtable >= 0.26.0, < 0.27dev',
+    'google-cloud-bigtable >= 0.29.0',
 ]
 
 SETUP_BASE.pop('url')
 
 setup(
     name='google-cloud-happybase',
-    version='0.26.0',
+    version='0.29.0',
     description='Client library for Google Cloud Bigtable: HappyBase layer',
     long_description=README,
     url='https://github.com/GoogleCloudPlatform/google-cloud-python-happybase',

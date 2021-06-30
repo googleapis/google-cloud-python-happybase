e371ece9215a379e28d5e3a0a5a51c40921b222f
Shubha Rajan <shubhadayini@google.com>
2021-06-29 19:28:35 -0700

    fix lint check

--- noxfile.py
+++ noxfile.py
@@ -40,6 +40,26 @@ def docs(session):
     )
 
 
+# Linting with flake8.
+#
+# We ignore the following rules:
+#   E203: whitespace before ‘:’
+#   E266: too many leading ‘#’ for block comment
+#   E501: line too long
+#   I202: Additional newline in a section of imports
+#
+# We also need to specify the rules which are ignored by default:
+# ['E226', 'W504', 'E126', 'E123', 'W503', 'E24', 'E704', 'E121']
+FLAKE8_COMMON_ARGS = [
+    "--show-source",
+    "--builtin=gettext",
+    "--max-complexity=20",
+    "--exclude=.nox,.cache,env,lib,generated_pb2,*_pb2.py,*_pb2_grpc.py",
+    "--ignore=E121,E123,E126,E203,E226,E24,E266,E501,E704,F401,W503,W504,I202",
+    "--max-line-length=88",
+]
+
+
 @nox.session(python="3.7")
 def lint(session):
     """Run linters.
@@ -48,7 +68,9 @@ def lint(session):
     """
     session.install("flake8", "black")
     session.run("black", "--check", "src", "docs", "unit_tests", "system_tests")
-    session.run("flake8", "google", "tests")
+    session.run(
+        "flake8", *FLAKE8_COMMON_ARGS, "src/google", "unit_tests", "system_tests"
+    )
 
 
 @nox.session(python="3.6")
